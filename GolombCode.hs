798195962957a5a312980920ef2ec010c84d6c09
lsb <lee@nodictionaries.com>
2012-09-15 15:12:16 +0000

    coding and decoding, no blocks

--- GolombCode.hs
+++ GolombCode.hs
@@ -1,6 +1,7 @@
-module GolombCode (golombCode, golombDecode, encodeUnary, decodeUnary, encodeBinary, decodeBinary) where
+module GolombCode where
 import Data.Tuple (swap)
 import Data.List (unfoldr, foldl', genericLength, genericReplicate, genericSplitAt)
+import Data.Word
 
 
 golombCode :: Integer -> Integer -> [Bool]
@@ -9,7 +10,8 @@ golombCode modulusBits integer = encodeUnary quotient ++ encodeBinary modulusBit
 
 encodeUnary quotient = (genericReplicate quotient True) ++ [False]
 
-encodeBinary modulusBits integer = reverse $ unfoldr (\ (mB,i) -> if mB == 0 then Nothing else Just (odd i, (mB-1, i `div` 2))) (modulusBits,integer)
+encodeBinary modulusBits integer = reverse $ unfoldr numToBools (modulusBits,integer)
+  where numToBools (mB,i) = if mB == 0 then Nothing else Just (odd i, (mB-1, i `div` 2))
 
 
 golombDecode :: Integer -> [Bool] -> (Integer, [Bool])
@@ -22,4 +24,28 @@ decodeUnary bits = (genericLength ones, rest)
 
 decodeBinary modulusBits bits = (int, rest)
   where (binaryBits, rest) = genericSplitAt modulusBits bits
-        int = foldl' (\ z e -> z * 2 + if e then 1 else 0) 0 binaryBits
+        int = boolsToInt binaryBits
+
+
+boolsToInt :: (Num a) => [Bool] -> a
+boolsToInt = foldl' (\ z e -> z*2 + (if e then 1 else 0)) 0
+
+boolsToBytes :: [Bool] -> Bool -> [Word8]
+boolsToBytes (b7:(b6:(b5:(b4:(b3:(b2:(b1:(b0:nextBytes)))))))) padding = (boolsToInt [b7,b6,b5,b4,b3,b2,b1,b0]) : (boolsToBytes nextBytes padding)
+boolsToBytes [] padding = []
+boolsToBytes fewBitsShort padding = [boolsToInt $ take 8 $ fewBitsShort ++ repeat padding]
+
+
+decodeMany 0 modulusBits bits = []
+decodeMany n modulusBits bits = i : (decodeMany (n-1) modulusBits rest)
+  where (i,rest) = golombDecode modulusBits bits
+
+
+golombCodesNoBlocks :: Integer -> [Integer] -> [Word8]
+golombCodesNoBlocks modulus ints = boolsToBytes (concatMap (golombCode modulus) ints) False
+
+golombDecodesNoBlocks :: Integer -> Integer -> [Word8] -> [Integer]
+golombDecodesNoBlocks modulusBits count words = decodeMany count modulusBits bits
+  where bits = concatMap (encodeBinary 8) words
+
+linewiseDiff ints = zipWith (\ a b -> a-b ) ints (0:ints)
