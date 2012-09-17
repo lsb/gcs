8bb7b90cf375b930df00f12361dabfefa5378aca
lsb <lee@nodictionaries.com>
2012-09-17 14:34:28 +0000

    remove unused code

--- golomb-compressed-sequences.hs
+++ golomb-compressed-sequences.hs
@@ -2,7 +2,6 @@ import GolombCode
 import System.Environment (getArgs)
 import qualified Data.ByteString.Lazy.Char8 as B
 import Data.Maybe (fromJust)
-import Debug.Trace (traceShow)
 
 main = do [modulusBits] <- getArgs
           B.interact (B.pack . map (toEnum . fromEnum) . golombCodesNoBlocks (read modulusBits) . linewiseDiff . map (fst . fromJust . B.readInteger) . B.lines)
