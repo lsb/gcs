a0b16d5adb1c603bf6fdc3ec642287a5a79fad8b
lsb <lee@nodictionaries.com>
2012-09-17 19:56:06 +0000

    predict how many bits to use for golomb unary encoding

--- mk-gcs.sh
+++ mk-gcs.sh
@@ -5,35 +5,29 @@ if [ -z $fp ]
 then
   fp=100
 fi
+unarybits=$2
+if [ -z $unarybits ]
+then
+  unarybits=`awk "BEGIN { print int(log($fp)/log(2)) }"`
+fi
 
 input=`mktemp`
 hashes=`mktemp`
-compressionsizes=`mktemp`
 
 gzip -c1 > $input
 linecount=`gzip -cd $input | wc -l`
 
-modulus=$(( linecount * fp ))
+modulus=$3
+if [ -z $modulus ]
+then
+  modulus=$(( linecount * fp ))
+fi
 
 #sort -n can't handle bigints
 export LC_ALL=C
 gzip -cd $input | ./make-hash-sequences $modulus | awk -v len=${#modulus} '{ printf("% " len "s\n",$1) }' | sort -S 50% -u | tr -d ' ' | gzip -c1 > $hashes
 
-for i in 4 5 6 7 8 9 10 11 ; do
-  seqname=`mktemp`
-  gzip -cd $hashes | ./golomb-compressed-sequences $i > $seqname
-  bytecount=`wc -c < $seqname`
-  echo -n $i@$(( $bytecount * 8000 / $linecount ))mbpc '/ ' > /dev/stderr
-  echo $i $bytecount $seqname >> $compressionsizes
-done
-
-minsize=`sort -n -k 2 < $compressionsizes | head -n 1`
-minmodbit=`echo $minsize | cut -d ' ' -f 1`
-minbytecount=`echo $minsize | cut -d ' ' -f 2`
-seqname=`echo $minsize | cut -d ' ' -f 3`
-
-(echo ; echo $minmodbit modulus bits, hashes modulo $modulus) > /dev/stderr
-
-cat $seqname
-
-rm `cut -d ' ' -f 3 < $compressionsizes` $input $hashes $compressionsizes
+outputlinecount=`gzip -cd $hashes | wc -l`
+echo "{'lineCount': ${outputlinecount}, 'modulus': ${modulus}, 'unaryBits': ${unarybits}}" > /dev/stderr
+gzip -cd $hashes | ./golomb-compressed-sequences $unarybits
+rm $input $hashes
