abbf32108fb791892be07a36e86903182d0272f3
lsb <lee@nodictionaries.com>
2012-09-18 22:26:38 +0000

    use tail-recursive golomb decoder that avoids making a linked list of boxed bools for 100x performance

--- GolombQuery.hs
+++ GolombQuery.hs
@@ -1,18 +1,35 @@
+{-# LANGUAGE BangPatterns #-}
 module GolombQuery where
-import GolombCode
 import HashSequence
 import Data.Word
 import qualified Data.ByteString.Lazy.Char8 as B
+import qualified Data.ByteString.Lazy as BW
 import Data.List.Ordered (isect)
 import Data.List (sort)
+import Data.Bits (testBit)
 
 linewiseSum :: [Int] -> [Integer]
 linewiseSum = scanl ((. fromIntegral) . (+)) 0
 
-golombFilterQueries :: Integer -> Integer -> Int -> [Word8] -> [B.ByteString] -> [B.ByteString]
-golombFilterQueries lineCount modulus unaryBits golombCodedSequence queries = filteredQueries
+golombFilterQueries :: Integer -> Integer -> Word8 -> BW.ByteString -> [B.ByteString] -> [B.ByteString]
+golombFilterQueries lineCount modulus binaryBits golombCodedSequence queries = filteredQueries
   where sortedHashes = sort [h | (q, h) <- queriesHashes]
         queriesHashes = [(q, hashMod modulus q) | q <- queries]
         filteredHashes = isect unencodedSequence sortedHashes
         filteredQueries = [q | (q,h) <- queriesHashes, h `elem` filteredHashes]
-        unencodedSequence = linewiseSum (golombDecodesNoBlocks unaryBits lineCount golombCodedSequence)
+        unencodedSequence = linewiseSum (fastGolombDecode lineCount binaryBits golombCodedSequence)
+
+fastGolombDecode :: Integer -> Word8 -> BW.ByteString -> [Int]
+fastGolombDecode !lineCount !binaryBits !bytes = fGDUnary (-1) 0 0 lineCount binaryBits bytes
+
+fGDUnary :: Int -> Word8 -> Int -> Integer -> Word8 -> BW.ByteString -> [Int]
+fGDUnary !bitIndex !currentWord !currentIntAccum 0 !binaryBytes !bytes = []
+fGDUnary (-1) !currentWord !currentIntAccum !intsRemaining !binaryBytes !bytes = fGDUnary 7 (BW.head bytes) currentIntAccum intsRemaining binaryBytes (BW.tail bytes)
+fGDUnary !bitIndex !currentWord !currentIntAccum !intsRemaining !binaryBytes !bytes = if currentWord `testBit` bitIndex
+                                                                                        then fGDUnary (bitIndex-1) currentWord (currentIntAccum+1) intsRemaining binaryBytes bytes
+                                                                                        else fGDBinary (bitIndex-1) currentWord binaryBytes currentIntAccum intsRemaining binaryBytes bytes
+
+fGDBinary :: Int -> Word8 -> Word8 -> Int -> Integer -> Word8 -> BW.ByteString -> [Int]
+fGDBinary !bitIndex !currentWord 0 !currentIntAccum !intsRemaining !binaryBytes !bytes = currentIntAccum : (fGDUnary bitIndex currentWord 0 (intsRemaining-1) binaryBytes bytes)
+fGDBinary (-1) !currentWord !intBitsRemaining !currentIntAccum !intsRemaining !binaryBytes !bytes = fGDBinary 7 (BW.head bytes) intBitsRemaining currentIntAccum intsRemaining binaryBytes (BW.tail bytes)
+fGDBinary !bitIndex !currentWord !intBitsRemaining !currentIntAccum !intsRemaining !binaryBytes !bytes = fGDBinary (bitIndex-1) currentWord (intBitsRemaining-1) (2*currentIntAccum + (if currentWord `testBit` bitIndex then 1 else 0)) intsRemaining binaryBytes bytes
--- golomb-query.hs
+++ golomb-query.hs
@@ -6,4 +6,4 @@ import qualified Data.ByteString.Lazy.Char8 as BChar
 main = do [lineCount, modulus, unaryBits, gcsFile] <- getArgs  -- todo: use the -index via aeson
           f <- BWord.readFile gcsFile
 	  queries <- BChar.getContents
-	  BChar.putStr $ BChar.unlines $ golombFilterQueries (read lineCount) (read modulus) (read unaryBits) (BWord.unpack f) (BChar.lines queries)
+	  BChar.putStr $ BChar.unlines $ golombFilterQueries (read lineCount) (read modulus) (read unaryBits) f (BChar.lines queries)
