9670f0def330482678acc618fc1801128349cec8
lsb <lee@nodictionaries.com>
2012-09-19 11:12:37 +0000

    calculate hashMod (w/ JS bigint, and MD5), map queries+thunks to hashes+thunks, and fuse the iterativeSum and isect into golomb decoding

--- GolombQuery.js
+++ GolombQuery.js
@@ -1,11 +1,22 @@
+function golombFilterQueries(lineCount, modulus, binaryBits, golombCodedSequence, queries) { // queries :: [(String, kWin :: IO (), kFail :: IO ())]
+    var queriesHashes = _.map(queries, function(s_kW_kF) { return [hashMod(modulus,string), s_kW_kF[1], s_kW_kF[2]] });
+    var sortedHashes = _.sortBy(queriesHashes, function(s_kW_kF) { return s_kW_kF[0] });
+    fastGolombDecodeIsectK(lineCount, binaryBits, golombCodedSequence, sortedHashes);
+}
+
+function hashMod(modulus, string) {
+    return BigInteger.parse(hex_md5(string),16).remainder(BigInteger(modulus)).toJSValue();
+    // we use JS integers (doubles) instead of big ints because even at 0.001% FP rate, you'd need well over a quadrillion values inserted into your sequence, by which point you'd probably need an index
+}
+
 // fastGolombDecode decodes lineCount bytes from the start of a base64-encoded golomb-coded sequence in the string bytes.
 // the code is identical to the haskell original except that the tail recursion has been hand-compiled to an imperative loop, and the input is base64-encoded.
 // (can you imagine starting off with half a dozen crazy different conditions, if you *didn't* have referential transparency?)
-function fastGolombDecode(lineCount, binaryBits, bytes) {
+function fastGolombDecodeIsectK(lineCount, binaryBits, bytes, queries) {
     var alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
     var mapping = {};
     alphabet.split(/(?=.)/).forEach(function(char,i) { mapping[char] = i });
-    var ints = new Array(lineCount);
+    var sequenceAccumulator = 0;
     var unary = true, intPtr = 0, bytePtr = 0, bitIndex = 0, currentWord = 0, intBitsRemaining = 0, currentIntAccum = 0, intsRemaining = 0;
     bitIndex = -1;
     intsRemaining = lineCount;
@@ -13,8 +24,17 @@ function fastGolombDecode(lineCount, binaryBits, bytes) {
 	// console.log(unary ? "unary" : "binary", "intPtr", intPtr, "bytePtr", bytePtr, "bitIndex", bitIndex, "currentWord", currentWord, "intBitsRemaining", intBitsRemaining, "currentIntAccum", currentIntAccum, "intsRemaining", intsRemaining);
 	if(!unary) {
 	    if(intBitsRemaining == 0) {
-		ints[intPtr] = currentIntAccum;
-		intPtr += 1;
+		sequenceAccumulator += currentIntAccum;
+		if(sequenceAccumulator > queries[0][0]) {
+		    queries[0][2]();
+		    queries.shift();
+		} else {
+		    while(sequenceAccumulator == queries[0][0]) {
+			queries[0][1]();
+			queries.shift();
+		    }
+		}
+		if(queries.length == 0) return;
 		unary = true;
 		// bitIndex stays as is
 		// currentWord stays as is
@@ -37,7 +57,12 @@ function fastGolombDecode(lineCount, binaryBits, bytes) {
 	    continue;
 	}
 	// unary is true, because of the continue
-	if(intsRemaining == 0) break;
+	if(intsRemaining == 0) {
+	    while(queries.length > 0) {
+		queries[0][2](); queries.shift();
+	    }
+	    return;
+	}
         if(bitIndex == -1) {
 	    bitIndex = 5;
 	    currentWord = mapping[bytes[bytePtr]];
@@ -57,5 +82,4 @@ function fastGolombDecode(lineCount, binaryBits, bytes) {
 	    // currentIntAccum stays as is
 	}
     }
-    return ints;
 }
