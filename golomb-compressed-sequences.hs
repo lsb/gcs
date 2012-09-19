2dea96dde327de56081ac3e249c782a8c6fa0d4e
lsb <lee@nodictionaries.com>
2012-09-19 05:21:03 +0000

    no longer use linked lists of boxed bools

--- golomb-compressed-sequences.hs
+++
@@ -1,7 +0,0 @@
-import GolombCode
-import System.Environment (getArgs)
-import qualified Data.ByteString.Lazy.Char8 as B
-import Data.Maybe (fromJust)
-
-main = do [binaryBits] <- getArgs
-          B.interact (B.pack . map (toEnum . fromEnum) . golombCodesNoBlocks (read binaryBits) . linewiseDiff . map (fst . fromJust . B.readInteger) . B.lines)
