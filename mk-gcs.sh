a9f0269ee440c2073f4c4345528ec3ad130edba2
lsb <lee@nodictionaries.com>
2012-09-20 04:22:03 +0000

    make a solid json blob and add indexing

--- mk-gcs.sh
+++ mk-gcs.sh
@@ -13,6 +13,7 @@ fi
 
 input=`mktemp`
 hashes=`mktemp`
+sequence=`mktemp`
 
 gzip -c1 > $input
 linecount=`gzip -cd $input | wc -l`
@@ -23,11 +24,26 @@ then
   modulus=$(( linecount * fp ))
 fi
 
+indexfrequency=$4
+if [ -z $indexfrequency ]
+then
+  indexfrequency=16384
+fi
+
 #sort -n can't handle bigints
 export LC_ALL=C
 gzip -cd $input | ./make-hash-sequences $modulus | awk -v len=${#modulus} '{ printf("% " len "s\n",$1) }' | sort -S 50% -u | tr -d ' ' | gzip -c1 > $hashes
 
+gzip -cd $hashes | ./golomb-encode $unarybits > $sequence
+
 outputlinecount=`gzip -cd $hashes | wc -l`
-echo "{'lineCount': ${outputlinecount}, 'modulus': ${modulus}, 'unaryBits': ${unarybits}}" > /dev/stderr
-gzip -cd $hashes | ./golomb-encode $unarybits
-rm $input $hashes
+(echo '{'
+  echo '"lineCount":' ${outputlinecount},
+  echo '"modulus":' ${modulus},
+  echo '"unaryBits":' ${unarybits},
+  echo -n '"partialSumBitcounts":' ; gzip -cd $hashes | ./make-index $unarybits $indexfrequency ; echo ,
+  echo -n '"b64EncodedGolombCodedSequence": "' ; ./base-64-encode < $sequence ; echo '"'
+echo '}') > /dev/stderr
+
+cat $sequence
+rm $input $hashes $sequence
