b2683b68c6cebfa0a412a6736af99df65931e812
lsb <lee@nodictionaries.com>
2012-09-20 13:46:21 +0000

    use getopts, at long last

--- mk-gcs.sh
+++ mk-gcs.sh
@@ -1,33 +1,40 @@
 #!/bin/sh
-
-fp=$1
-if [ -z $fp ]
-then
-  fp=100
-fi
-unarybits=$2
-if [ -z $binarybits ]
-then
-  binarybits=`awk "BEGIN { print int(log($fp)/log(2)) }"`
-fi
-
 input=`mktemp`
 hashes=`mktemp`
 sequence=`mktemp`
-
 gzip -c1 > $input
 linecount=`gzip -cd $input | wc -l`
 
-modulus=$3
-if [ -z $modulus ]
-then
-  modulus=$(( linecount * fp ))
-fi
+fp=100
+binarybits=
+modulus=$(( linecount * fp ))
+indexfrequency=16384
+while getopts "hp:b:m:i:" OPTION
+do
+    case $OPTION in
+	h)
+	    echo "usage: $0 -p false-positive-rate (default: 100) -b bits-for-binary-Rice-encoding (default: floor(log2(fp))) -m hash-modulus (default: linecount * fp) -i index-stride (default: 16k)"
+	    echo "stdin: one key per line. stdout: golomb-compressed sequence. stderr: json with base64-encoded gcs and index."
+	    exit 1
+	    ;;
+	p)
+	    fp=$OPTARG
+	    ;;
+	b)
+	    binarybits=$OPTARG
+	    ;;
+	m)
+	    modulus=$OPTARG
+	    ;;
+	i)
+	    indexfrequency=$OPTARG
+	    ;;
+    esac
+done
 
-indexfrequency=$4
-if [ -z $indexfrequency ]
+if [ -z $binarybits ]
 then
-  indexfrequency=16384
+    binarybits=`awk "BEGIN { print int(log($fp)/log(2)) }"`
 fi
 
 #sort -n can't handle bigints
@@ -44,6 +51,5 @@ outputlinecount=`gzip -cd $hashes | wc -l`
   echo -n '"partialSumBitcounts":' ; gzip -cd $hashes | ./make-index $binarybits $indexfrequency ; echo ,
   echo -n '"b64EncodedGolombCodedSequence": "' ; ./base-64-encode < $sequence ; echo '"'
 echo '}') > /dev/stderr
-
 cat $sequence
 rm $input $hashes $sequence
